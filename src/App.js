74f560ce84e5e66e4365929d3086c4a3aa915e60
Tringapps-Sathish <102800173+Tringapps-Sathish@users.noreply.github.com>
2022-10-21 10:56:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './task/task4.css';
 import Form from './task/form';
 
 function App() {
-  localStorage.setItem("Shop 1",JSON.stringify([{pro: "Realme", qua : "1", pri : 10000, tot : 10000 }]));
-  localStorage.setItem("Shop 2",JSON.stringify([{pro: "OnePlus", qua : "1", pri : 25000, tot : 25000 }]));
-  localStorage.setItem("Shop 3",JSON.stringify([{pro: "AC", qua : "1", pri : 20000, tot : 20000 }]));
+  localStorage.setItem("Shop",JSON.stringify([
+    {pro: "Realme", qua : "1", pri : 10000, tot : 10000 },
+    {pro: "OnePlus", qua : "1", pri : 25000, tot : 25000 }, 
+    {pro: "AC", qua : "1", pri : 20000, tot : 20000 }
+  ]));
+
 
   return (
     <div className="App">
